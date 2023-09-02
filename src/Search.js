4a4794da1baa66a2ceed9ed260b2354552b3431d
samthemogultech <samthemogul.tech@gmail.com>
2023-09-02 02:45:01 +0100

    new add

---
+++ src/Search.js
@@ -0,0 +1,17 @@
+
+
+const Search = ({searchfield, searchChange}) => {
+    return (
+        <div className="pa2">
+            <input 
+                className="pa3 ba b--green bg-lightest-blue"
+                type='search' 
+                placeholder="search robots"
+                onChange={searchChange}
+            />
+        </div>
+        
+    );
+}
+
+export default Search;
